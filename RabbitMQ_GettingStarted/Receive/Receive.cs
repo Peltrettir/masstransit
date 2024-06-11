c0c3e4b1df7c8af0e472ac2e00f7af425f80ba6b
r.peltretti <r.peltretti@xeel.tech>
2024-06-11 16:00:44 +0200

    RabbitMQ HelloWorld

---
+++ RabbitMQ_GettingStarted/Receive/Receive.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("Hello", false, false, false, null);
+
+Console.WriteLine("[*] Waiting for messages");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Received message \"{message}\"");
+};
+
+channel.BasicConsume("Hello", true, consumer);
+
+Console.ReadLine();
---
+++ RabbitMQ_GettingStarted/Send/Send.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("Hello", false, false, false, null);
+const string message = "Hello, World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "Hello", null, body);
+
+Console.WriteLine($"Sent message \"{message}\"");
+Console.ReadLine();
